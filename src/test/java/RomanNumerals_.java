6886f27b4d4ae5bfc76428a6b76a52f4cc4b7096
Néstor Ojeda González <nestorojedagonzalez@gmail.com>
2019-09-23 12:26:32 +0100

    first iteration (case 0)

--- src/test/java/RomanNumerals_.java
+++ src/test/java/RomanNumerals_.java
@@ -1,23 +1,37 @@
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
+import org.omg.PortableInterceptor.ServerRequestInfo;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(org.junit.runners.Parameterized.class)
 public class RomanNumerals_ {
 
-    private final int numbere;
+    private final int number;
     private final String value;
     private final boolean exceptionExpected;
 
     public RomanNumerals_(int numbere, String value, boolean exceptionExpected) {
-        this.numbere = numbere;
+        this.number = numbere;
         this.value = value;
         this.exceptionExpected = exceptionExpected;
     }
 
     @Test
     public void execute(){
-        
+        try {
+            assertThat(toRoman(number)).isEqualTo(this.value);
+            assertFalse(exceptionExpected);
+        }catch (Exception e){
+            assertTrue(exceptionExpected);
+        }
+    }
+
+    private String toRoman(int number){
+        throw new IllegalParameterException();
     }
 
     @Parameterized.Parameters
@@ -25,6 +39,10 @@ public class RomanNumerals_ {
         return new Object[][]{
                 {0, null, true}
 
+
         };
+    }
+
+    private static class IllegalParameterException extends RuntimeException {
     }
 }
