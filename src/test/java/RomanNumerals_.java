0ab52f476ca9f3dd7b00e392de171cddbf9f15f0
Néstor Ojeda González <nestorojedagonzalez@gmail.com>
2019-09-23 12:08:03 +0100

    basic scheme of testing class

--- src/test/java/RomanNumerals_.java
+++ src/test/java/RomanNumerals_.java
@@ -1,6 +1,30 @@
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
 @RunWith(org.junit.runners.Parameterized.class)
 public class RomanNumerals_ {
 
+    private final int numbere;
+    private final String value;
+    private final boolean exceptionExpected;
+
+    public RomanNumerals_(int numbere, String value, boolean exceptionExpected) {
+        this.numbere = numbere;
+        this.value = value;
+        this.exceptionExpected = exceptionExpected;
+    }
+
+    @Test
+    public void execute(){
+        
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] cases(){
+        return new Object[][]{
+                {0, null, true}
+
+        };
+    }
 }
